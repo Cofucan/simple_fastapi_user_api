582d0217b3ad2aae0d20e4932d48b48689d51dcb
Uche Ofia <coolucheofia@gmail.com>
2023-09-15 17:45:26 +0100

    test: Update test script

--- test_api.py
+++ test_api.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 
 # Replace with your server URL
 BASE_URL = "http://web-01.cofucan.tech/api"
@@ -8,7 +9,7 @@ BASE_URL = "http://web-01.cofucan.tech/api"
 def print_response(response):
     print(f"Status Code: {response.status_code}")
     print("Response:")
-    print(response.text)
+    pprint(response.text)
     print()
 
 
@@ -22,47 +23,52 @@ new_user_data = {
 }
 
 response = requests.post(f"{BASE_URL}/", json=new_user_data)
+count = 0
+
+# Extract the user ID from the response
+while response.status_code != 200 and count < 5:
+    response = requests.post(f"{BASE_URL}/", json=new_user_data)
+    count += 1
+
 print("Create User:")
 print_response(response)
 
-# Extract the user ID from the response
-if response.status_code == 200:
-    created_user = response.json()
-    user_id = created_user["id"]
+created_user = response.json()
+user_id = created_user["id"]
 
-    # Test getting the user by ID
-    response = requests.get(f"{BASE_URL}/{user_id}")
-    print("Get User by ID:")
-    print_response(response)
+# Test getting the user by ID
+response = requests.get(f"{BASE_URL}/{user_id}")
+print("Get User by ID:")
+print_response(response)
 
-    # Test updating a user
-    update_data = {
-        "name": "Updated User",
-        "email": "updated@example.com"
-    }
+# Test updating a user
+update_data = {
+    "name": "Updated User",
+    "email": "updated@example.com",
+    "stage": 2
+}
 
-    response = requests.put(f"{BASE_URL}/{user_id}", json=update_data)
-    print("Update User:")
-    print_response(response)
-
-    # Test deleting the user by ID
-    response = requests.delete(f"{BASE_URL}/{user_id}")
-    print("Delete User by ID:")
-    print_response(response)
-else:
-    print("Failed to create user. Skipping Get and Delete operations.")
-
-# Test getting all users
-response = requests.get(f"{BASE_URL}/")
-print("Get All Users:")
+response = requests.put(f"{BASE_URL}/{user_id}", json=update_data)
+print("Update User:")
 print_response(response)
 
 # Test filtering users
 filter_criteria = {
-    "username": "testuser",
-    "stage": 0
+    "gender": "M"
 }
 
 response = requests.get(f"{BASE_URL}/", json=filter_criteria)
 print("Get Users with Filtering Criteria:")
+print_response(response)
+
+# Test deleting the user by ID
+response = requests.delete(f"{BASE_URL}/{user_id}")
+print("Delete User by ID:")
+print_response(response)
+
+# print("Failed to create user. Skipping Get and Delete operations.")
+
+# Test getting all users
+response = requests.get(f"{BASE_URL}/")
+print("Get All Users:")
 print_response(response)
