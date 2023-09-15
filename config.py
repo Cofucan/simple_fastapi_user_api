d36891e35ffad82f645612a92e961aac6c74a000
Uche Ofia <coolucheofia@gmail.com>
2023-09-15 01:10:13 +0100

    fix: Update config variables

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-MYSQL_USER: str = config.get("mysql", "user")
-MYSQL_PASSWORD = config.get("mysql", "password")
-SERVER_IP = config.get("mysql", "server_ip")
-DB_NAME = config.get("mysql", "db_name")
+MYSQL_USER: str = config.get("mysql", "mysql_user")
+MYSQL_PASSWORD: str = config.get("mysql", "mysql_password")
+SERVER_IP: str = config.get("mysql", "server_ip")
+DB_NAME: str = config.get("mysql", "db_name")
